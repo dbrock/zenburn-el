7a3de3161d13ca629ed89e63834470f29b27e0bd
Stefan Kangas <skangas@skangas.se>
2010-09-22 16:41:19 +0200

    Fix diff-mode background.

--- zenburn.el
+++ zenburn.el
@@ -494,6 +494,7 @@ static char *gnus-pointer[] = {
      '(diff-added ((t (:inherit zenburn-primary-3))))
      '(diff-removed ((t (:inherit zenburn-blue))))
      '(diff-context ((t (:inherit font-lock-comment))))
+     '(diff-refine-change ((t (:inherit zenburn-background-2))))
 
      `(emms-pbi-song ((t (:foreground ,zenburn-yellow))))
      '(emms-pbi-current ((t (:inherit zenburn-primary-1))))
@@ -927,6 +928,7 @@ static char *gnus-pointer[] = {
        diff-hunk-header-face
        diff-index-face
        diff-removed-face
+       diff-refine-change-face
        emms-pbi-current-face
        emms-pbi-mark-marked-face
        emms-pbi-song-face
